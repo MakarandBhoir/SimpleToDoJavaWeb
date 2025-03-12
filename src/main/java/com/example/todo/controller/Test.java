31adae36d0357c3a1ff6e8e7bf2810922f036dba
makarand.bhoir <makarand.bhoir@live.com>
2025-03-12 21:26:36 +0530

    dummy change-1

--- src/main/java/com/example/todo/controller/Test.java
+++ src/main/java/com/example/todo/controller/Test.java
@@ -4,6 +4,7 @@ import java.util.logging.Logger;
 
 public class Test {
     // Create a duplicate code so that sonar cloud can detect it
+    // dummy change
     Logger logger = Logger.getLogger(Test.class.getName());
 
     public void duplicateCode() {

6c39c917bb709a3cd26f53f363d453c984e6c05e
makarand.bhoir <makarand.bhoir@live.com>
2025-03-12 21:56:43 +0530

    Todo changes

--- src/main/java/com/example/todo/model/Todo.java
+++ src/main/java/com/example/todo/model/Todo.java
@@ -1,9 +1,24 @@
 package com.example.todo.model;
 
 public class Todo {
-    public String id;
-    public String title;
-    public boolean completed;
+    private String id;
+    private String title;
+    private boolean completed;
+
+    // Getter method for id
+    public String getId() {
+        return id;
+    }
+
+    // Getter method for title
+    public String getTitle() {
+        return title;
+    }
+
+    // Getter method for completed
+    public boolean isCompleted() {
+        return completed;
+    }
 
     // Constructor
     public Todo(String id, String title, boolean completed) {
